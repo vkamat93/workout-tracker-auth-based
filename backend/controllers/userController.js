6f69ecd637e06ecdc45346cfebd41aae67031dbf
vkamat93 <vikrant.p.kamat@gmail.com>
2026-01-29 20:38:34 +0530

    Added login user logic

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,7 +8,17 @@ const createToken = (_id) => {
 
 // login user
 const loginUser = async (req, res) => {
-    res.json({ message: 'login user' });
+    const { email, password } = req.body;
+    
+    try {
+        const existingUser = await User.login(email, password); // creates a Mongo DB document
+        // create a token
+        const token = createToken(existingUser._id);
+
+        res.status(200).json({email, token})
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
 }
 
 
@@ -17,7 +27,6 @@ const signupUser = async (req, res) => {
     const { email, password } = req.body;
     try {
         const newUser = await User.signup(email, password); // creates a Mongo DB document
-
         // create a token
         const token = createToken(newUser._id);
 
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -14,7 +14,7 @@ const userSchema = new mongoose.Schema({
     }
 })
 
-// creating static method to be called in controller
+// creating static Signup method to be called in controller
 userSchema.statics.signup = async function (email, password) {
 
     // validation logic
@@ -28,18 +28,38 @@ userSchema.statics.signup = async function (email, password) {
         throw Error('Password should be alphanumeric');
     }
 
-
-
-    // cannot user User.findOne since we are exporting it after this LOC and exporting it elsewhere, so this is used
-    const userExists = await this.findOne({ email }) 
+    // cannot use User.findOne since we are exporting it after this LOC and exporting it elsewhere, so this is used
+    const userExists = await this.findOne({ email })
     if (userExists) {
         throw Error('Email already in use');
     }
 
     const hashedPassword = await bcrypt.hash(password, 10); // salt value of 10
 
-    const user = await this.create({email, password: hashedPassword})
+    const user = await this.create({ email, password: hashedPassword })
     return user;
+}
+
+// creating static Login method to be called in controller
+userSchema.statics.login = async function (email, password) {
+
+    // validation logic
+    if (!email || !password) {
+        throw Error('All fields must be filled');
+    }
+
+    // cannot use User.findOne since we are exporting it after this LOC and exporting it elsewhere, so this is used
+    const legitUser = await this.findOne({ email })
+    if (!legitUser) {
+        throw Error('Incorrect credentials'); // generic message to avoid giving hints
+    }
+
+    const match = await bcrypt.compare(password, legitUser.password);
+    if (!match) {
+        throw Error('Incorrect credentials'); // generic message to avoid giving hints
+    }
+
+    return legitUser;
 }
 
 module.exports = mongoose.model('User', userSchema);
