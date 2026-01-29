a30f586355a0dcfce1828c287a400521159e5df6
vkamat93 <vikrant.p.kamat@gmail.com>
2026-01-30 00:40:19 +0530

    Assign workouts to specific users
    clear global workouts state by useWorkoutsContext hook dispatch

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,7 +1,9 @@
 import { useAuthContext } from './useAuthContext'
+import {useWorkoutsContext} from './useWorkoutsContext'
 
 const useLogout = () => {
     const { dispatch } = useAuthContext()
+    const {dispatch: workoutsDispatch} = useWorkoutsContext()
 
     const logout = () => {
         // remove user from local storage
@@ -9,6 +11,9 @@ const useLogout = () => {
 
         // dispatch logout action
         dispatch({type: 'LOGOUT'})
+        
+        // clear the global workouts state on logout to avoid flashing previous user's workouts
+        workoutsDispatch({type: 'SET_WORKOUTS', payload: null})
     }
 
     return { logout }
