dcf1b3eb7f76d7d15b74c016d8be404d9c912465
vkamat93 <vikrant.p.kamat@gmail.com>
2026-01-29 22:53:04 +0530

    Implement useSignup hook and use it in Signup component

---
+++ frontend/src/hooks/useSignup.js
@@ -0,0 +1,35 @@
+import { useState } from "react"
+import { useAuthContext } from "./useAuthContext"
+
+export const useSignup = () => {
+    const [error, setError] = useState(null)
+    const [isLoading, setIsLoading] = useState(null)
+    const {dispatch} = useAuthContext()
+
+    const signup = async (email, password) => {
+        setIsLoading(true)
+        setError(null) // evrytime we call signup, reset error
+
+        const response = await fetch('/api/user/signup', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({email, password})
+        })
+        const userData = await response.json()
+
+        if (!response.ok) {
+            setIsLoading(false)
+            setError(data.error)
+        }
+        if (response.ok) {
+            // save user to local storage
+            localStorage.setItem('userData', JSON.stringify(userData))
+
+            // update auth context
+            dispatch({type: 'LOGIN', payload: userData})
+            setIsLoading(false)
+        }
+    }
+
+    return {signup, isLoading, error}
+}
--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,14 +1,16 @@
 import { useState } from 'react'
+import { useSignup } from '../hooks/useSignup'
 
 const Signup = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const {signup, isLoading, error} = useSignup()
 
     const handleSubmit = async (e) => {
         // when form is submitted, default behavior is refreshing the page
         e.preventDefault()
-        
-        console.log(email, password)
+
+        await signup(email, password)
     }
 
     return (
@@ -26,7 +28,8 @@ const Signup = () => {
                 onChange={(e) => setPassword(e.target.value)}
                 value={password}
             />
-            <button>Sign up</button>
+            <button disabled={isLoading}>Sign up</button>
+            {error && <div className='error'>{error}</div>}
         </form>
     )
 }
