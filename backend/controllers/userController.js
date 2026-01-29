582e97b46893f728bc35d259dc3f0dbc7fe3529d
vkamat93 <vikrant.p.kamat@gmail.com>
2026-01-29 20:04:47 +0530

    Add signup basic logic and password hashing

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,6 +8,14 @@ const loginUser = async (req, res) => {
 
 // signup user
 const signupUser = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await user.signup(email, password);
+        res.status(200).json({email, user})
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+
     res.json({ message: 'signup user' });
 }
 
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -11,5 +12,19 @@ const userSchema = new mongoose.Schema({
         required: true,
     }
 })
+
+// creating static method to be called in controller
+userSchema.statics.signup = async function (email, password) {
+    // cannot user User.findOne since we are exporting it after this LOC and exporting it elsewhere, so this is used
+    const userExists = await this.findOne({ email }) 
+    if (userExists) {
+        throw Error('Email already in use');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10); // salt value of 10
+
+    const user = await this.create({email, password: hashedPassword})
+    return user;
+}
 
 module.exports = mongoose.model('User', userSchema);
