5bcd079ef9e53c226e31d2aa9c148ee9633f7b75
vkamat93 <vikrant.p.kamat@gmail.com>
2026-01-30 00:21:09 +0530

    Making authorized workout details requests - Fix errors

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -4,13 +4,13 @@ const User = require('../models/userModel')
 const requireAuth = async (req, res, next) => {
 
     // verify if user is authenticated
-    const {authorizationHeader} = req.headers
+    const {authorization} = req.headers
 
-    if (!authorizationHeader) {
+    if (!authorization) {
         return res.status(401).json({error: 'Authorization token required'})
     }
 
-    const token = authorizationHeader.split(' ')[1]
+    const token = authorization.split(' ')[1]
 
     try {
         const {_id} = jwt.verify(token, process.env.SECRET_KEY)
